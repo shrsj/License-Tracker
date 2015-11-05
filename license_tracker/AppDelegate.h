bb4b4ad395a96f65808cf05e02cf9067b10a0291
shravan <shravan@shravans-MacBook-Pro.local>
2015-11-06 02:41:33 +0530

    Initial Commit

---
+++ license_tracker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  license_tracker
+//
+//  Created by shravan on 06/11/15.
+//  Copyright (c) 2015 shravan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ license_tracker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  license_tracker
+//
+//  Created by shravan on 06/11/15.
+//  Copyright (c) 2015 shravan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
